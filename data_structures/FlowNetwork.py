50ceed8894123ea28821979ec7bf41df430f8c45
Calvin Ho <18680207+calvh@users.noreply.github.com>
2020-12-19 20:35:46 -0500

    Add capacity function to FlowNetwork

--- data_structures/FlowNetwork.py
+++ data_structures/FlowNetwork.py
@@ -4,32 +4,34 @@ from data_structures.AdjacencyListDigraph import *
 class FlowNetwork(AdjacencyListDigraph):
     """Flow network implemented using an adjacency-list-based graph"""
 
-    def __init__(self, digraph, capacity_function, source, sink):
-
-        # todo find out more on this
-        # self.capacity_function = capacity_function
+    def __init__(self, digraph, capacities, source, sink):
 
         self.vertices = digraph.vertices
         self.edges = digraph.edges
+
         self.source = source
         self.sink = sink
+
+        # the capacity function specified in the parameter capacities is a dictionary which maps edges (tuples) to capacities
+        self.capacities = capacities
 
     def set_capacity(self, v1, v2, c):
 
         if c < 0:
             raise ValueError("Capacity must be non-negative.")
-        self.set_edge_field(v1, v2, capacity=c)
+
+        self.capacities[(v1, v2)] = c
+
+    def get_capacity(self, v1, v2):
+        return self.capacities[(v1, v2)]
 
     def set_flow(self, v1, v2, f):
 
-        if f > self.get_capacity(v1, v2):
+        if f > self.capacities[(v1, v2)]:
             raise ValueError(
-                f"Flow cannot exceed capacity of {self.get_capacity(v1, v2)}"
+                f"Flow cannot exceed capacity of {self.capacities[(v1, v2)]}"
             )
         self.set_edge_field(v1, v2, flow=f)
-
-    def get_capacity(self, v1, v2):
-        return self.get_edge_field(v1, v2, "capacity")
 
     def get_flow(self, v1, v2):
         return self.get_edge_field(v1, v2, "flow")
--- tests/test_flow_network.py
+++ tests/test_flow_network.py
@@ -6,23 +6,34 @@ g = AdjacencyListDigraph()
 for i in range(0, 7):
     g.add_vertex(i)
 
-g.add_edge(0, 1, weight=1)
-g.add_edge(0, 2, weight=1)
-g.add_edge(0, 3, weight=1)
-g.add_edge(1, 3, weight=1)
-g.add_edge(1, 4, weight=1)
-g.add_edge(2, 3, weight=1)
-g.add_edge(2, 5, weight=1)
-g.add_edge(3, 4, weight=1)
-g.add_edge(3, 6, weight=1)
-g.add_edge(4, 6, weight=1)
-g.add_edge(5, 6, weight=1)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(0, 3)
+g.add_edge(1, 3)
+g.add_edge(1, 4)
+g.add_edge(2, 3)
+g.add_edge(2, 5)
+g.add_edge(3, 4)
+g.add_edge(3, 6)
+g.add_edge(4, 6)
+g.add_edge(5, 6)
 
-n = FlowNetwork(g, None, 0, 6)
+capacity_function = {
+    (0, 1): 7,
+    (0, 2): 6,
+    (0, 3): 5,
+    (1, 3): 1,
+    (1, 4): 2,
+    (2, 3): 3,
+    (2, 5): 9,
+    (3, 4): 5,
+    (3, 6): 3,
+    (4, 6): 6,
+    (5, 6): 8,
+}
 
-n.set_capacity(0, 1, 10)
-n.set_flow(0, 1, 10)
+n = FlowNetwork(g, capacity_function, 0, 6)
 
-print(n.get_flow(0, 1))
+n.set_capacity(0, 1, 999)
 print(n.get_capacity(0, 1))
 print(n)
