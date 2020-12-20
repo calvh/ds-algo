d5f5c3294b2d81fa6235171657d7ecfd7405b09f
Calvin Ho <18680207+calvh@users.noreply.github.com>
2020-12-19 19:27:08 -0500

    Modify Bellman-Ford test

--- tests/test_bellman_ford_shortest.py
+++ tests/test_bellman_ford_shortest.py
@@ -1,35 +1,81 @@
 from algorithms.graphs.bellman_ford_shortest import *
-from data_structures.AdjacencyListDigraph import *
-from copy import deepcopy
+from data_structures.AdjacencyListDigraph import AdjacencyListDigraph
 
-g_acyclic = AdjacencyListDigraph()
 
-for c in "ABCDEF":
-    g_acyclic.add_vertex(c)
+def test_acyclic():
+    # test graph with no negative-weight cycle
+    g = AdjacencyListDigraph()
 
-# test graph with no negative-weight cycle
-g_acyclic.add_edge("A", "B", weight=8)
-g_acyclic.add_edge("A", "C", weight=-2)
-g_acyclic.add_edge("A", "D", weight=4)
-g_acyclic.add_edge("B", "E", weight=-2)
-g_acyclic.add_edge("C", "B", weight=7)
-g_acyclic.add_edge("C", "D", weight=1)
-g_acyclic.add_edge("C", "E", weight=3)
-g_acyclic.add_edge("D", "F", weight=5)
-g_acyclic.add_edge("F", "C", weight=9)
+    for c in "ABCDEF":
+        g.add_vertex(c)
 
-# test graph with negative-weight cycle
-g_negative_cycle = deepcopy(g_acyclic)
-g_negative_cycle.set_edge_field("C", "D", weight=-1)
-g_negative_cycle.set_edge_field("D", "F", weight=-5)
-g_negative_cycle.set_edge_field("F", "C", weight=-9)
+    g.add_edge("A", "B", weight=8)
+    g.add_edge("A", "C", weight=-2)
+    g.add_edge("A", "D", weight=4)
+    g.add_edge("B", "E", weight=-2)
+    g.add_edge("C", "B", weight=7)
+    g.add_edge("C", "D", weight=1)
+    g.add_edge("C", "E", weight=3)
+    g.add_edge("D", "F", weight=5)
+    g.add_edge("F", "C", weight=9)
 
-# acyclic
-acyclic_distances = bellman_ford_shortest(g_acyclic, "A")
-print(acyclic_distances)
-print(g_acyclic)
+    # acyclic
+    acyclic_distances = bellman_ford_shortest(g, "A")
+    print(acyclic_distances)
+    print(g)
 
-# negative-weight cycle
-negative_cycle_distances = bellman_ford_shortest(g_negative_cycle, "A")
-print(negative_cycle_distances)
-print(g_negative_cycle)
+
+def test_negative_cycle():
+    # test graph with negative-weight cycle
+
+    g = AdjacencyListDigraph()
+
+    for c in "ABCDEF":
+        g.add_vertex(c)
+
+    g.add_edge("A", "B", weight=8)
+    g.add_edge("A", "C", weight=-2)
+    g.add_edge("A", "D", weight=4)
+    g.add_edge("B", "E", weight=-2)
+    g.add_edge("C", "B", weight=7)
+    g.add_edge("C", "E", weight=3)
+    g.add_edge("C", "D", weight=-1)
+    g.add_edge("D", "F", weight=-5)
+    g.add_edge("F", "C", weight=-9)
+
+    # negative-weight cycle
+    negative_cycle_distances = bellman_ford_shortest(g, "A")
+    print(negative_cycle_distances)
+    print(g)
+
+
+def test_airport():
+    # test graph with no negative-weight cycle
+    g = AdjacencyListDigraph()
+
+    g.add_vertex("BWI")
+    g.add_vertex("JFK")
+    g.add_vertex("MIA")
+    g.add_vertex("ORD")
+    g.add_vertex("DFW")
+    g.add_vertex("LAX")
+
+    g.add_edge("BWI", "JFK", weight=10)
+    g.add_edge("BWI", "MIA", weight=20)
+    g.add_edge("JFK", "ORD", weight=-8)
+    g.add_edge("JFK", "DFW", weight=-10)
+    g.add_edge("MIA", "JFK", weight=-15)
+    g.add_edge("MIA", "DFW", weight=-25)
+    g.add_edge("MIA", "LAX", weight=30)
+    g.add_edge("ORD", "BWI", weight=20)
+    g.add_edge("ORD", "DFW", weight=-10)
+    g.add_edge("ORD", "LAX", weight=3)
+    g.add_edge("DFW", "LAX", weight=12)
+
+    distances = bellman_ford_shortest(g, "BWI")
+    print(distances)
+    print(g)
+
+test_acyclic()
+test_negative_cycle()
+test_airport()
